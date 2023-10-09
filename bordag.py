6ef925e8630530df67f94ea2b5bb287926af5da2
Arne Rubehn <arne.rubehn@student.uni-tuebingen.de>
2023-10-09 11:22:48 +0200

    added stubs for implementing Bordag (2008).

---
+++ bordag.py
@@ -0,0 +1,13 @@
+"""
+Workflow:
+
+VANILLA
+1. preliminary segmentation by LSV (ideally from words with similar function, maybe POS tags?)
+2. use these segmentations to "train" prefix and suffix tries
+
+REFINED
+1. split compounds
+2. iterate LSV algorithm
+3. refine trie classification by the means of certain restrictions
+
+"""
